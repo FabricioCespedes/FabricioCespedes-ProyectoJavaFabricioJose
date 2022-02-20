b898e3a169b125bdfa0dc1bc8157a3ad54145b27
Jose0810 <joseandresvillalobos0@gmail.com>
2022-02-20 17:44:00 -0600

    Cadena de conexión funcional

---
+++ src/main/java/AccesoDatos/Config.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AccesoDatos;
+
+/**
+ *
+ * @author josea
+ */
+public class Config {
+    /**
+     * Método que tiene la cadena de conexión con la base de datos
+     * @return La cadena de conexión
+     */
+    public static String getConnectionString(){
+        return "jdbc:sqlserver://localhost:1433;databaseName=DBCronograma;user=javaUser;password=java";
+    }
+}
